bc0fdb03892625d3d5bc7a523b8371aa14090fc8
luna1998 <56982800+luna1998@users.noreply.github.com>
2021-11-15 20:19:36 -0500

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1,6 +1,6 @@
 class Portfolio:
     def _init_(self):
-        self._stocks=[]
+        self._stocks=[] #加下划线是因为设置成了private
 
     def buy(self,str name,int shares,float price):
         self._stocks.append((name,shares,price))
