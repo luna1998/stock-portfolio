36e498376d4fa9fe9d97e07b1f7439bd2246acc8
luna1998 <56982800+luna1998@users.noreply.github.com>
2021-11-15 20:01:18 -0500

    Create portfolio.py
    define class portfolio and several methods

---
+++ portfolio.py
@@ -0,0 +1,7 @@
+class Portfolio:
+    def buy(self,str name,int shares,float price):
+        self.name=name
+        self.shares=shares
+        self.price=price
+    def cost():
+        return float(buy.shares * buy.price)
